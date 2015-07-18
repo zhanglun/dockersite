14b98d9cab59fdb0a2f3a6ca3fa1a6c3b0b976f6
zhanglun <zhanglun1410@gmail.com>
2015-07-18 21:03:24 +0800

    update serer

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var path = require('path');
+var http = require('http');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var moment = require('moment');
 var mongoose = require('mongoose');
 
+var debug = require('debug')('express-demo:server');
+
 // Constants
-var PORT = 80;
+var PORT = 3000;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -74,6 +83,89 @@ app.use(function(err, req, res, next) {
 
 
 
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen();
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
+
+
 
 var port = process.env.MONGODB_PORT_27017_TCP_PORT;
 var addr = process.env.MONGODB_PORT_27017_TCP_ADDR;
@@ -82,8 +174,8 @@ var password = process.env.MONGODB_PASSWORD;
 var username = process.env.MONGODB_USERNAME;
 
 // 'mongodb://user:pass@localhost:port/database'
-mongoose.connect('mongodb://' + username + ':' + password +'@' + addr + ':' + port + '/' + instance);
-var Records = mongoose.model('Records', { name: {type: String, default:'any'}, time: {type: Date, default: Date.now} });
+// mongoose.connect('mongodb://' + username + ':' + password +'@' + addr + ':' + port + '/' + instance);
+// var Records = mongoose.model('Records', { name: {type: String, default:'any'}, time: {type: Date, default: Date.now} });
 
 
 
@@ -106,5 +198,10 @@ var Records = mongoose.model('Records', { name: {type: String, default:'any'}, t
 
 
 
-app.listen(PORT);
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+server.listen(PORT);
 console.log('Master Running on http://localhost:' + PORT);
