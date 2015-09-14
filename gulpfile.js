644f2e1e3ec47f62932aae6eba0304f9ffd02c63
zhanglun <zhanglun1410@gmail.com>
2015-09-14 17:54:44 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp = require('gulp'),
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-    return gulp.src('./src/scss/*.scss', {
+     gulp.src('./src/scss/todo.scss', {
             sourcemap: true
         })
         .pipe(plumber())
