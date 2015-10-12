aaa7a4a8f1fd2a2be4e33a9eb55cf501ada21e8b
zhanglun <zhanglun1410@gmail.com>
2015-10-12 20:48:18 +0800

    commit

--- app/blog/lib/api.controller.js
+++ app/blog/lib/api.controller.js
@@ -144,15 +144,16 @@ var temp = null;
 router.get('/kuaipan/request_token', function (req, res, next) {
   var promise = Kuaipan.getRequestToken();
   promise.then(function (result) {
-    console.log('result');
-    console.log(result);
-    result = result[0];
-    var body = JSON.parse(result.body);
-    req.session.oauth_token_secret = body.oauth_token_secret;
-    res.send({
-      code: result.statusCode,
-      body: body
-    });
+    //console.log('result');
+    //console.log(result.length);
+    //result = result[1];
+    //var body = JSON.parse(result.body);
+    //req.session.oauth_token_secret = body.oauth_token_secret;
+    //res.send({
+    //  code: result.statusCode,
+    //  body: body
+    //});
+    res.send(result);
   })
     .catch(function (err) {
       console.log(err);
