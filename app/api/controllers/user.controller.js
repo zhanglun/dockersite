be51ccc0bd7bf961db35df4b3aa2b388acf6fd7b
zhanglun <zhanglun1410@gmail.com>
2015-12-28 23:02:54 +0800

    update

--- app/api/controllers/user.controller.js
+++ app/api/controllers/user.controller.js
@@ -6,22 +6,31 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
+  function (username, password, done) {
+    User.findOne({username: username}, function (err, user) {
+      if (err) {
+        return done(err);
+      }
       if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
+        return done(null, false, {message: 'Incorrect username.'});
       }
       if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
+        return done(null, false, {message: 'Incorrect password.'});
       }
       return done(null, user);
     });
   }
 ));
 
+passport.serializeUser(function (user, done) {//保存user对象
+  done(null, user);//可以通过数据库方式操作
+});
 
-module.exports = function(app) {
+passport.deserializeUser(function (user, done) {//删除user对象
+  done(null, user);//可以通过数据库方式操作
+});
+
+module.exports = function (app) {
   app.use('/api/user', router);
 };
 
@@ -32,7 +41,7 @@ var UserHandler = {};
 /**
  * 获取用户信息
  */
-UserHandler.getUserDetail = function(uid) {
+UserHandler.getUserDetail = function (uid) {
 
 };
 
@@ -40,23 +49,77 @@ UserHandler.getUserDetail = function(uid) {
  * authenticate user
  * @param uid
  */
-UserHandler.autheniticate = function(uid) {
+UserHandler.autheniticate = function (uid) {
 
 };
-router.get('/:username', function(req, res) {
 
-  var username = req.param('username'); // /:username
+/*
+ Helper Functions
+ */
+function authenticate(name, pass, fn) {
+  if (!module.parent) console.log('authenticating %s:%s', name, pass);
+
+  User.findOne({
+      username: name
+    },
+
+    function (err, user) {
+      if (user) {
+        if (err) return fn(new Error('cannot find user'));
+        hash(pass, user.salt, function (err, hash) {
+          if (err) return fn(err);
+          if (hash == user.hash) return fn(null, user);
+          fn(new Error('invalid password'));
+        });
+      } else {
+        return fn(new Error('cannot find user'));
+      }
+    });
+
+}
+
+function requiredAuthentication(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.session.error = 'Access denied!';
+    res.redirect('/login');
+  }
+}
+
+function userExist(req, res, next) {
+  User.count({
+    username: req.body.username
+  }, function (err, count) {
+    if (count === 0) {
+      next();
+    } else {
+      req.session.error = "User Exist";
+      res.redirect("/signup");
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+router.get('/:username', function (req, res) {
+
 
   var param = req.params; // {username: /:username}
   var data = req.body;
-  res.status(200).json({
-    name: username
-  });
+  res.status(200).json(param);
 });
 
 
-
-router.post('/authenticate', function(req, res) {
+router.post('/authenticate', function (req, res) {
   console.log('user authenticate');
   res.send(200).json({
     param: req.param,
@@ -64,6 +127,13 @@ router.post('/authenticate', function(req, res) {
   })
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
+router.post('/login', passport.authenticate('local'), function (req, res) {
   res.redirect('/user/' + req.user.username);
+});
+
+router.post('/signup', function (req, res) {
+  var password = req.body.password;
+  var username = req.body.username;
+  res.status(200).json(req.body);
+
 });
