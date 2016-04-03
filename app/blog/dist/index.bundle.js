007ed42bbd320b3e56ed5039eced02a1f094cf12
zhanglun <zhanglun1410@gmail.com>
2016-04-03 11:04:26 +0800

    markdown with allowAtxHeaderWithoutSpace

--- app/blog/dist/index.bundle.js
+++ app/blog/dist/index.bundle.js
@@ -1536,6 +1536,7 @@
 	 */
 	var marked = __webpack_require__(4);
 	var util = __webpack_require__(6);
+	// var Ace = require('ace');
 	
 	var contentMarked = function contentMarked(val) {
 	  return marked(val);
@@ -1551,8 +1552,11 @@
 	  var metadata = '# MarkNote for you\n' + '# MarkNote for you' + '\n' + '## MarkNote for you' + '\n' + '### MarkNote for you' + '\n' + '#### MarkNote for you' + '\n' + '##### MarkNote for you' + '\n' + '###### MarkNote for you' + '\n' + '![file-list](https://www.zybuluo.com/static/img/file-list.png)' + '\n\n' + '- [ ] 支持以 PDF 格式导出文稿' + '\n' + '- [ ] 改进 Cmd 渲染算法，使用局部渲染技术提高渲染效率' + '\n' + '- [x] 新增 Todo 列表功能' + '\n' + '- [x] 修复 LaTex 公式渲染问题' + '\n' + '- [x] 新增 LaTex 公式编号功能' + '\n\n' + '```js\n' + 'let Code=()=>{\n' + '  console.log("hello world");\n' + '}\n' + '```\n';
 	
 	  var marknote = CodeMirror(container, {
-	    // value: metadata,
-	    mode: "markdown",
+	    value: metadata,
+	    mode: {
+	      name: 'markdown',
+	      allowAtxHeaderWithoutSpace: true
+	    },
 	    tabSize: 2,
 	    lineWrapping: true,
 	    theme: 'tomorrow-night-eighties',
--- app/blog/src/js/marknote.js
+++ app/blog/src/js/marknote.js
@@ -3,12 +3,13 @@
  */
 var marked = require('marked');
 var util = require('./util');
+// var Ace = require('ace');
 
-var contentMarked = function (val) {
+var contentMarked = function(val) {
   return marked(val);
 };
 
-function Editor(container){
+function Editor(container) {
   // var metadata = '++++' + '\n' +
   //   'title: ' + '\n' +
   //   'category: ' + '\n' +
@@ -17,31 +18,34 @@ function Editor(container){
   //   '++++' + '\n';
   var metadata = '# MarkNote for you\n' +
 
-  '# MarkNote for you' + '\n' +
-  '## MarkNote for you' + '\n' +
-  '### MarkNote for you' + '\n' +
-  '#### MarkNote for you' + '\n' +
-  '##### MarkNote for you' + '\n' +
-  '###### MarkNote for you' + '\n' +
-  '![file-list](https://www.zybuluo.com/static/img/file-list.png)' +'\n\n' +
-  '- [ ] 支持以 PDF 格式导出文稿' + '\n' +
-  '- [ ] 改进 Cmd 渲染算法，使用局部渲染技术提高渲染效率' + '\n' +
-  '- [x] 新增 Todo 列表功能' + '\n' +
-  '- [x] 修复 LaTex 公式渲染问题' + '\n' +
-  '- [x] 新增 LaTex 公式编号功能' + '\n\n' +
-  '```js\n' +
-  'let Code=()=>{\n' +
-  '  console.log("hello world");\n' +
-  '}\n' +
-  '```\n';
+    '# MarkNote for you' + '\n' +
+    '## MarkNote for you' + '\n' +
+    '### MarkNote for you' + '\n' +
+    '#### MarkNote for you' + '\n' +
+    '##### MarkNote for you' + '\n' +
+    '###### MarkNote for you' + '\n' +
+    '![file-list](https://www.zybuluo.com/static/img/file-list.png)' + '\n\n' +
+    '- [ ] 支持以 PDF 格式导出文稿' + '\n' +
+    '- [ ] 改进 Cmd 渲染算法，使用局部渲染技术提高渲染效率' + '\n' +
+    '- [x] 新增 Todo 列表功能' + '\n' +
+    '- [x] 修复 LaTex 公式渲染问题' + '\n' +
+    '- [x] 新增 LaTex 公式编号功能' + '\n\n' +
+    '```js\n' +
+    'let Code=()=>{\n' +
+    '  console.log("hello world");\n' +
+    '}\n' +
+    '```\n';
 
   var marknote = CodeMirror(container, {
-    // value: metadata,
-    mode: "markdown",
-    tabSize:2,
+    value: metadata,
+    mode: {
+      name: 'markdown',
+      allowAtxHeaderWithoutSpace: true
+    },
+    tabSize: 2,
     lineWrapping: true,
     theme: 'tomorrow-night-eighties',
-    autofocus: true
+    autofocus: true,
   });
 
   marknote.setOption("extraKeys", {
@@ -56,17 +60,17 @@ function Editor(container){
 }
 
 
-var editor = function () {
+var editor = function() {
   return new Vue({
     el: '#blog-editor',
-    ready: function () {
+    ready: function() {
       var _this = this;
       var editorContainer = document.getElementById('writer-board');
       var editor = Editor(editorContainer);
       _this.editor = editor;
       util.getCategoryList()
-        .then(function (res) {
-          var _temp = res.map(function (item) {
+        .then(function(res) {
+          var _temp = res.map(function(item) {
             return item['category'];
           });
           _this.$set('categories', _temp);
