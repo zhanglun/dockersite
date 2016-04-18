0e06356c3399ccdef5f2ab1c83cb38a9b34fa8b2
zhanglun <zhanglun1410@gmail.com>
2016-04-18 23:14:33 +0800

    format code

--- app/blog/services/blog.service.js
+++ app/blog/services/blog.service.js
@@ -3,23 +3,23 @@ var Moment = require('moment');
 
 var Blog = {};
 
-function convertObjectIdToId(target){
-  if(Array.isArray(target)){
-    target = target.map(function(item){
+function convertObjectIdToId(target) {
+  if (Array.isArray(target)) {
+    target = target.map(function (item) {
       item.id = item._id;
       delete item._id;
       delete item.__v;
       return item
     });
-  }else{
-      target.id = target._id;
-      delete target.__v;
-      delete target._id;
+  } else {
+    target.id = target._id;
+    delete target.__v;
+    delete target._id;
   }
   return target;
 }
 
-Blog.getArticleList = function(){
+Blog.getArticleList = function () {
   return db.Article.find({}).execAsync()
     .then(function (list) {
       list = list.map(function (item) {
@@ -30,56 +30,56 @@ Blog.getArticleList = function(){
       });
       list = convertObjectIdToId(list);
       return list;
-  });
+    });
 };
 
 
-Blog.getArticleDetail = function(id){
+Blog.getArticleDetail = function (id) {
   return db.Article.findOneAsync({'_id': id})
-    .then(function(article){
-        article = article.toObject();
-        article.ctime = Moment(article.ctime).format('YYYY-MM-DD HH:mm:ss');
-        article.utime = Moment(article.utime).format('YYYY-MM-DD HH:mm:ss');
-        article = convertObjectIdToId(article);
-        return article;
+    .then(function (article) {
+      article = article.toObject();
+      article.ctime = Moment(article.ctime).format('YYYY-MM-DD HH:mm:ss');
+      article.utime = Moment(article.utime).format('YYYY-MM-DD HH:mm:ss');
+      article = convertObjectIdToId(article);
+      return article;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
     });
-}
+};
 
-Blog.createArticle = function(article){
+Blog.createArticle = function (article) {
   var newArticle = db.Article(article);
   return newArticle.saveAsync()
-    .then(function(result){
+    .then(function (result) {
       // result -> [article, 1]
       article = convertObjectIdToId(result[0]);
       return article;
     })
-    .catch(function(err){
+    .catch(function (err) {
       return err;
     });
-}
+};
 
-Blog.modifyArticle = function(id, update){
-  return db.Article.findByIdAndUpdateAsync(id, update, {new :true})
-    .then(function(article){
+Blog.modifyArticle = function (id, update) {
+  return db.Article.findByIdAndUpdateAsync(id, update, {new: true})
+    .then(function (article) {
       article = convertObjectIdToId(article);
       return article;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
       return err;
     });
-}
+};
 
-Blog.deleteArticle = function(id){
+Blog.deleteArticle = function (id) {
   return db.Article.findOneAndRemoveAsync({_id: id})
-    .then(function(article){
+    .then(function (article) {
       article = convertObjectIdToId(article);
       return article;
     })
-    .catch(function(err){
+    .catch(function (err) {
       return err;
     });
 };
